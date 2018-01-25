508433a6f395d35ab1e75e15df1ed182ec4af236
jyotisubhra <33259027+jyotisubhra@users.noreply.github.com>
2018-01-25 15:17:12 +0000

    2nd tutorial

---
+++ SpringTutorial2/src/org/jyoti/javamaster/Triangle.java
@@ -0,0 +1,20 @@
+package org.jyoti.javamaster;
+
+public class Triangle {
+	private String type;
+	
+	
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public void draw() {
+		System.out.println(getType() + " Draw Triangle");
+	}
+}
