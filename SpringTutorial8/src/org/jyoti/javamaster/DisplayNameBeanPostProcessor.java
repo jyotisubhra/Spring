96d62849423337d69b9b4b8fafcee24c608b3487
jyotisubhra <33259027+jyotisubhra@users.noreply.github.com>
2018-01-26 11:49:46 +0000

    Writing a BeanPostProcessor

---
+++ SpringTutorial8/src/org/jyoti/javamaster/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,19 @@
+package org.jyoti.javamaster;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("in Post Process After Init " + beanName);
+		return bean;
+	}
+	
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("in Post Process Before Init " + beanName);
+		return bean;
+	}
+}
